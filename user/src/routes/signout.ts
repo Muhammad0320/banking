f73be49d4875d0778dffe18ed0bc65da8db259f9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 18:36:07 +0100

    chore: update signout ts

--- user/src/routes/signout.ts
+++ user/src/routes/signout.ts
@@ -1,5 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { currentUser } from '../../middleware/currentUser';
+import { requireAuth } from '../../middleware/requireAuth';
 
 const router = express.Router();
+
+router.post(
+  '/signout',
+  currentUser,
+  requireAuth,
+  async (req: Request, res: Response) => {
+    req.currentUser = null;
+
+    res
+      .status(200)
+      .json({ status: 'success', data: 'Logged out successfully' });
+  }
+);
 
 export { router as signoutRouter };

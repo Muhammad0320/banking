e60341d229bc7a59508222152a5f0bfafbcf3beb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 11:50:45 +0100

    chore: add user schema

--- user/model/user.ts
+++ user/model/user.ts
@@ -1,23 +1,64 @@
-import mongoose from "mongoose";
-
-
-
+import mongoose from 'mongoose';
 
 type UserAttrs = {
+  name: string;
+  email: string;
+  password: string;
+  passwordConfirm: string;
+  status: string;
+  role: string;
+  createdAt: Date;
+};
 
-    name: string;
-    email: string;
-    password: string;
-    passwordConfirm: string;
-    status: string ;
+type UserDoc = mongoose.Document & UserAttrs & { version: number };
 
-}
+type UserModel = mongoose.Model<UserDoc> & {
+  buildUser: (attrs: UserAttrs) => Promise<UserDoc>;
+};
 
-type UserDoc = mongoose.Document & UserAttrs & {version: number} 
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'This field is required.'],
+    minlength: [4, 'Name should be at least 4 chars.']
+  },
 
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'This field is required.']
+  },
 
-type UserModel = mongoose.Model<UserDoc> &  {
+  password: {
+    type: String,
+    required: [true, 'This field is required.'],
+    minlength: [8, 'Your password should be more than 8 chars.'],
+    select: false
+  },
 
-    buildUser: (attrs: UserAttrs) => Promise<UserDoc>
+  passwordConfirm: {
+    type: String,
+    validate: {
+      validator: function(this: UserDoc, value: string): boolean {
+        return this.password === value;
+      },
 
-}
+      message: 'Passwords are not the same'
+    }
+  },
+
+  role: {
+    type: String,
+    required: [true, 'This field is required']
+  },
+
+  status: {
+    type: String,
+    required: [true, 'This field is required']
+  },
+
+  createdAt: {
+    type: String,
+    default: new Date()
+  }
+});

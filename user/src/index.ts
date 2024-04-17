b448e23596d8d729474b5979c1f400800ac570d6
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 04:52:29 +0100

    chore:  update index ts file

--- user/src/index.ts
+++ user/src/index.ts
@@ -1,4 +1,6 @@
-const start = () => {
+import mongoose from 'mongoose';
+
+const start = async () => {
   const port = 3000;
 
   if (!process.env.JWT_KEY) {
@@ -7,6 +9,14 @@ const start = () => {
 
   if (!process.env.MONGO_URI) {
     throw new Error(' MONGO_URI not found ');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log('connected to mongoDB');
+  } catch (error) {
+    console.error(error);
   }
 };
 

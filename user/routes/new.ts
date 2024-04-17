05de8d044eb441557dee90c867b6e27544956c73
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 12:47:01 +0100

    chore: update new ts

--- user/routes/new.ts
+++ user/routes/new.ts
@@ -1,5 +1,20 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import User from '../model/user';
 
 const router = express.Router();
+
+router.post('/', async (req: Request, res: Response) => {
+  const { email, ...attrs } = req.body;
+
+  const existingUser = await User.findOne({ email });
+
+  if (!!existingUser) {
+    throw new Error('Bad request Error');
+  }
+
+  const user = await User.buildUser({ ...attrs, email });
+
+  return res.status(201).json({ status: 'success', data: user });
+});
 
 export { router as createUserRouter };

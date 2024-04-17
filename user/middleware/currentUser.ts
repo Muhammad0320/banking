1ac6c99d01a6cd9e0c012ea571b1b99fff48229b
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 18:00:37 +0100

    chore: add currentUser as a request object

--- user/middleware/currentUser.ts
+++ user/middleware/currentUser.ts
@@ -1,5 +1,17 @@
 import { NextFunction, Request, Response } from 'express';
 
+interface UserPayload {
+  id: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser: UserPayload;
+    }
+  }
+}
+
 export const currentUser = (
   req: Request,
   res: Response,

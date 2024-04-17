797c096c35ec5e1b4148eb87c7592ab77d8bb10e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 17:53:21 +0100

    chore:  update signin ts

--- user/src/routes/signin.ts
+++ user/src/routes/signin.ts
@@ -1,7 +1,44 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { emailValidator, passwordValidator } from '../services/validators';
+import User from '../model/user';
+import { Passwords } from '../services/Password';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
-router.post('/signin');
+router.post(
+  '/signin',
+  emailValidator,
+  passwordValidator,
+  passwordValidator,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email }).select('+password');
+
+    if (!existingUser) {
+      throw new Error(' Not authorized ');
+    }
+
+    const isCorrectPassword = await Passwords.compare(
+      existingUser.password,
+      password
+    );
+
+    if (!isCorrectPassword) {
+      throw new Error(' Not authorized ');
+    }
+
+    const token = jwt.sign({ id: existingUser.id }, process.env.JWT_KEY!, {
+      expiresIn: +process.env.JWT_EXPIRES_IN! * 60 * 60
+    });
+
+    req.session = {
+      jwt: token
+    };
+
+    res.status(200).json({ status: 'success', data: existingUser });
+  }
+);
 
 export { router as signinRouter };

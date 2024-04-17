21465432e272df94acbb2fd901667424810930e0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 15:08:31 +0100

    chore: update signup ts

--- user/src/routes/signup.ts
+++ user/src/routes/signup.ts
@@ -1,32 +1,22 @@
 import express, { Request, Response } from 'express';
 import User from '../model/user';
 import { body } from 'express-validator';
+import {
+  emailValidator,
+  nameValidator,
+  passwordConfirmationValidator,
+  passwordValidator
+} from '../services/validators';
 
 const router = express.Router();
 
 router.post(
   '/',
   [
-    body('name')
-      .trim()
-      .notEmpty()
-
-      .isString()
-      .withMessage('Please provide a valid name'),
-    body('email')
-      .trim()
-      .notEmpty()
-      .isEmail()
-      .withMessage('Please provide a valid email'),
-    body('password')
-      .trim()
-      .notEmpty()
-      .isString()
-      .withMessage('Please provide a valid password'),
-    body('passwordConfirm')
-      .trim()
-      .notEmpty()
-      .withMessage('Comfirm your password')
+    nameValidator,
+    emailValidator,
+    passwordValidator,
+    passwordConfirmationValidator
   ],
   async (req: Request, res: Response) => {
     const { email, ...attrs } = req.body;

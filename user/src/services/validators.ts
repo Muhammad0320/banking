cd5e06a0f808195b89f41d33175155926d28f4d7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 15:07:16 +0100

    chore: add resuable validators

---
+++ user/src/services/validators.ts
@@ -0,0 +1,30 @@
+import { body } from 'express-validator';
+
+export const nameValidator = () =>
+  body('name')
+    .trim()
+    .notEmpty()
+
+    .isString()
+    .withMessage('Please provide a valid name');
+
+export const emailValidator = () =>
+  body('email')
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .withMessage('Please provide a valid email');
+
+export const passwordValidator = () =>
+  body('password')
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage('Please provide a valid password');
+
+export const passwordConfirmationValidator = () =>
+  body('passwordConfirm')
+    .trim()
+    .notEmpty()
+    .isString()
+    .withMessage('Comfirm your password');

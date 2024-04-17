71a98cc3c9a64a268d395e837169c13e70e79bd2
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 12:39:54 +0100

    chore: udate app ts

--- user/src/app.ts
+++ user/src/app.ts
@@ -1,5 +1,16 @@
+import cookieSession from 'cookie-session';
 import express from 'express';
 
 const app = express();
+
+app.set('trust proxy', true);
+
+app.use(
+  cookieSession({
+    httpOnly: true,
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
 
 export { app };

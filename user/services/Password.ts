09952424043cb9ea5a74f2c833dc2003981a10ab
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-17 12:16:49 +0100

    chore: update password class

---
+++ user/services/Password.ts
@@ -0,0 +1,30 @@
+import { randomBytes, scrypt } from 'crypto';
+import { promisify } from 'util';
+
+interface PasswordType {
+  hash(password: string): Promise<string>;
+  compare(storedPassword: string, suppliedPassword: string): Promise<boolean>;
+}
+
+const scryptAsync = promisify(scrypt);
+
+export class Passwords implements PasswordType {
+  async hash(password: string): Promise<string> {
+    const salt = randomBytes(8).toString('hex');
+
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+
+    return ` ${buf.toString('hex')}.${salt}`;
+  }
+
+  async compare(
+    storedPassword: string,
+    suppliedPassword: string
+  ): Promise<boolean> {
+    const [hashedPassword, salt] = storedPassword.split('.');
+
+    const buf = (await scryptAsync(suppliedPassword, salt, 64)) as Buffer;
+
+    return buf.toString('hex') === hashedPassword;
+  }
+}

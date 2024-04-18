76aad065e111a25b306634039acbde93639a2a07
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-18 06:51:40 +0100

    chore:  update not found error

--- user/error/NotFound.ts
+++ user/error/NotFound.ts
@@ -3,13 +3,13 @@ import { CustomError } from './CustomError';
 export class NotFound extends CustomError {
   statusCode = 404;
 
-  constructor() {
+  constructor(public message: string) {
     super();
 
     Object.setPrototypeOf(this, NotFound.prototype);
   }
 
-  serializeError(message: string) {
-    return [{ message }];
+  serializeError() {
+    return [{ message: this.message }];
   }
 }

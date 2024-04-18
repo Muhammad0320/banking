06a9b50a6496ba1a4881e10d9b60dddedbe5155e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-18 07:27:12 +0100

    chore: update the requestValidator moddleware

--- user/middleware/requestValidator.ts
+++ user/middleware/requestValidator.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request } from 'express';
 import { validationResult } from 'express-validator';
+import { RequestValidation } from '../error/RequestValidation';
 
 export const requestValidator = (req: Request, _: any, next: NextFunction) => {
-  const error = validationResult(req);
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    throw new RequestValidation(errors.array());
+  }
+
+  next();
 };

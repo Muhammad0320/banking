1a751c366c03533cc084d55f741a9da00651ddfb
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-18 06:58:51 +0100

    chore:  update notAuthorized error

--- user/error/NotAuthorized.ts
+++ user/error/NotAuthorized.ts
@@ -1,3 +1,15 @@
 import { CustomError } from './CustomError';
 
-export class NotAuthorized extends CustomError {}
+export class NotAuthorized extends CustomError {
+  statusCode = 401;
+
+  constructor() {
+    super();
+
+    Object.setPrototypeOf(this, NotAuthorized.prototype);
+  }
+
+  serializeError() {
+    return [{ message: 'Not Authorized, Please signin' }];
+  }
+}

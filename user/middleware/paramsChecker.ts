f2bd972e5a7ee08d1fe70ce963804e5aafa5301f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-18 07:57:49 +0100

    chore:  add params chcker

--- user/middleware/paramsChecker.ts
+++ user/middleware/paramsChecker.ts
@@ -1,5 +1,12 @@
 import { NextFunction, Request, Response } from 'express';
+import { BadRequest } from '../error/BadRequest';
 
-export const paramsChecker = (params: string) => {
-  return (req: Request, res: Response, next: NextFunction) => {};
+export const paramsChecker = (param: string) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const params = req.params[param];
+
+    if (!params) return new BadRequest('Please provide a valid param  ');
+
+    next();
+  };
 };

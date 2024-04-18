1bf91bdd45a2c03926d160716546ad10883fd3a0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-18 06:38:58 +0100

    chore:  update custom error file

--- user/error/CustomError.ts
+++ user/error/CustomError.ts
@@ -1,7 +1,16 @@
-export class CustomError extends Error {
+interface ErrorObject {
+  message: string;
+  field?: string;
+}
+
+export abstract class CustomError extends Error {
+  abstract statusCode: number;
+
   constructor() {
     super();
 
     Object.setPrototypeOf(this, CustomError.prototype);
   }
+
+  abstract serializeError(): ErrorObject[];
 }

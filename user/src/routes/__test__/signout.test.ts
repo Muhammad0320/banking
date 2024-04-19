f3430454f8a46d42be545e1c6271b9dfce228136
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-19 09:13:50 +0100

    chore: add test that

--- user/src/routes/__test__/signout.test.ts
+++ user/src/routes/__test__/signout.test.ts
@@ -7,3 +7,13 @@ it('returns a 401, if unauthenticated user tried to logout', async () => {
     .send({})
     .expect(401);
 });
+
+it('returns a 200, if the user is authenticated', async () => {
+  const cookie = await global.signin();
+
+  await request(app)
+    .post('/api/v1/user/signout')
+    .set('Cookie', cookie)
+    .send({})
+    .expect(200);
+});

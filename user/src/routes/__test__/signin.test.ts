365ce4804f184d6fc774c9c1343d3798079cf6a9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-19 06:53:11 +0100

    chore: update signin ts

--- user/src/routes/__test__/signin.test.ts
+++ user/src/routes/__test__/signin.test.ts
@@ -58,3 +58,26 @@ it('returns a 400 on incorrect password', async () => {
     })
     .expect(400);
 });
+
+it('returns a 200 on valid inputs', async () => {
+  const {
+    body: { data }
+  } = await request(app)
+    .post('/api/v1/user/signup')
+    .send({
+      name: 'shit man',
+      email: 'shitman@gmail.com',
+      password: 'shijgtnjngnrgnr',
+      passwordConfirm: 'shijgtnjngnrgnr',
+      status: 'shit'
+    })
+    .expect(200);
+
+  await request(app)
+    .post('/api/v1/user/signin')
+    .send({
+      email: data.email,
+      password: 'shijgtnjngnrgnr'
+    })
+    .expect(200);
+});

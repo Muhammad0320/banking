190800d27cefd5c1c4d5f7e5ec6e8cade3e7b395
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-20 09:33:24 +0100

    chore: update index ts

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -8,18 +8,6 @@ const start = async () => {
     throw new Error(' JWT_KEY not found ');
   }
 
-  if (!process.env.MONGO_URI) {
-    throw new Error(' MONGO_URI not found ');
-  }
-
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log('connected to mongoDB');
-  } catch (error) {
-    console.error(error);
-  }
-
   app.listen(port, () => {
     console.log('App running on port ', port);
   });

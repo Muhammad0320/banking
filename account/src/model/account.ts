9f24929c77ac8b426c257d0884c2ca8856e00891
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-20 07:31:49 +0100

    chore: add user schema

--- account/src/model/account.ts
+++ account/src/model/account.ts
@@ -11,18 +11,22 @@ enum AccountCurrency {
   USD = 'USD'
 }
 
+enum AccountStatus {
+  Frozen = 'frozen',
+  Blocked = 'blocked',
+  Active = 'active'
+}
+
 type AccountAttrs = {
   balance: number;
 
-  status: string;
+  status: AccountStatus;
 
   userId: string;
 
   tier: string;
 
   type: AccountType;
-
-  createdAt: Date;
 
   currency: AccountCurrency;
 };
@@ -32,3 +36,47 @@ type AccountDoc = mongoose.Document & AccountAttrs & { version: number };
 type AccountModel = mongoose.Model<AccountDoc> & {
   buildAccount(attrs: AccountAttrs): Promise<AccountDoc>;
 };
+
+const accountSchema = new mongoose.Schema({
+  balance: {
+    type: String,
+
+    required: true
+  },
+
+  status: {
+    type: String,
+
+    required: true,
+
+    enum: Object.values(AccountStatus)
+  },
+
+  tier: {
+    type: String,
+    required: true
+  },
+
+  type: {
+    type: String,
+    required: true,
+    enum: Object.values(AccountType)
+  },
+
+  currency: {
+    type: String,
+    required: true,
+    enum: Object.values(AccountCurrency)
+  },
+
+  userId: {
+    type: String,
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    required: true,
+    default: new Date()
+  }
+});
